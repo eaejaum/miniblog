ae274a92f94998609f4e11664a2b552c1bb21f7f
eaejaum <jpedroalmeida179@gmail.com>
2024-09-20 23:37:27 -0300

    rota para individual post

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import Dashboard from './pages/Dashboard/Dashboard';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 function App() {
    const [user, setUser] = useState(undefined);
@@ -41,6 +42,7 @@ function App() {
               <Route path='/' element={<Home />}/>
               <Route path='/about' element={<About />}/>
               <Route path='/search' element={<Search />}/>
+              <Route path='/posts/:id' element={<Post />}/>
               <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />}/>
               <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />}/>
               <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to='/' />}/>
